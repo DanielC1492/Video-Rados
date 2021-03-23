8bf31d8911a6ff1e5c96f14a612f45b9f956011e
Daniel Castillo <danielcastillog92@gmail.com>
2021-03-23 19:39:51 +0100

    Home view added

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='homeDiv'>
+            Vista home del maravilloso mundo de RADOS
+        </div>
+    )
+}
+
+export default Home
