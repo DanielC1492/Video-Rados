f618fc29243e74c74c1bbb3e2bbabaa3f36ad53a
Daniel Castillo <danielcastillog92@gmail.com>
2021-03-23 20:38:49 +0100

    Movie poster component added. Some examples in home view given.

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import MoviePoster from '../../components/MoviePoster/MoviePoster'
 import './Home.css'
 
 const Home = () => {
     return (
         <div className='homeDiv'>
             Vista home del maravilloso mundo de RADOS
+            <MoviePoster/>
         </div>
     )
 }
