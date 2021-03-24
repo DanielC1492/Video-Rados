376b134b04099933b4e8fdf55d703fcefad7b6dc
Daniel Castillo <danielcastillog92@gmail.com>
2021-03-24 17:31:05 +0100

    Button fixed

--- src/components/MyButton/MyButton.jsx
+++ src/components/MyButton/MyButton.jsx
@@ -1,28 +1,25 @@
 
 import React from 'react';
-import './MoviePoster.css';
+import './MyButton.css';
+import {useHistory} from 'react-router-dom';
 
-const MoviePoster = (props) => {
+const MyButton = (props) => {
 
-    // const history = useHistory();
+     const history = useHistory();
+
+     const redirectMe = () => {
+         history.push(`/${props.destination}`)
+     }
 
 
 
     return (
-        <div className='posterContainer'>
-           <div className='posterDiv'>
-               <img className='posterImage' src='https://i.pinimg.com/originals/5d/24/59/5d245987e8fd014b24ff6bf99808960a.jpg'></img>
-           </div>
-           <div className='posterDiv'>
-           <img className='posterImage' src='https://static.posters.cz/image/750/poster/avengers-endgame-journey-s-end-i73600.jpg'></img>
-           </div>
-           <div className='posterDiv'>
-           <img className='posterImage' src='https://images-na.ssl-images-amazon.com/images/I/41rG0YoMJXL._AC_.jpg'></img>
-           </div>
+        <div onClick={() => redirectMe()} className='buttonStyle'>
+        
         </div>
     )
 }
 
 
-export default MoviePoster
+export default MyButton;
 
