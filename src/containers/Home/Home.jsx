c830ff568ef014bf0145b5e0b1f0689991bea64f
Fede <fedebaez@gmail.com>
2021-03-27 20:00:07 +0100

    Added banner to home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,13 +3,19 @@ import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import MovieCarousel from '../../components/MovieCarousel/MovieCarousel';
 import './Home.css'
+import gif from '../../img/announcement.gif';
 
 const Home = () => {
     return (
         <>
         <Header/>
         <div className='homeDiv'>
+            <div className="banner">
+                <div className="gif"><img src={gif} alt='Anuncio'/></div>
+                <div className="text"><b>¡Atención! Mañana 28/12/1990</b> - Proyección en exclusiva del estreno <b>"Solo en Casa"</b> en nuestro establecimiento de la calle Emilio Baró en Benimaclet. - <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Apúntate aquí</a></div>
+            </div>
             
+            <h3>Elige una película y prueba ahora nuestro innovador sistema de visionado en línea</h3>
             <h4>Mejor valoradas</h4>
             <MovieCarousel query='toprated=true'/>
             <h4>Más populares</h4>
