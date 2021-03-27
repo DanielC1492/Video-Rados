2ee6d2b91303c773607ce19c700a3e821376f0f9
Daniel Castillo <danielcastillog92@gmail.com>
2021-03-27 20:35:09 +0100

    Movie almost finished. Just some fixes left.

--- src/containers/Movie/Movie.jsx
+++ src/containers/Movie/Movie.jsx
@@ -5,6 +5,8 @@ import MyButton from '../../components/MyButton/MyButton';
 import { Rate } from 'antd';
 import { connect } from 'react-redux';
 import Header from '../../components/Header/Header';
+import MoviePoster from '../../components/MoviePoster/MoviePoster';
+import playButton from '../../img/playButton.png';
 
 
 
@@ -17,7 +19,9 @@ const Movie = (props) => {
             
             <div className='midLeft'>
                 <div className='midLeftTop'>
-                    <div className='trailer'>TRAILER</div>
+                    <div className='trailer'>
+                        <img src={playButton}></img>
+                    </div>
                 </div>  
                 <div className='midLeftBot'>
                     <div className='title'>
@@ -28,17 +32,12 @@ const Movie = (props) => {
                     
                     <div className='ratingDuration'>
                         <div className='empty'></div>
-                        <div className='ratingStars'><Rate allowHalf disabled defaultValue={2.5} /></div>
-                        <div className='duration'>Duración: 120min</div>
+                        <div className='ratingStars'><Rate allowHalf count={10} disabled defaultValue={props.movie.vote_average} /></div>
+                        <div className='price'>Precio: 2.99€/Día</div>
                     </div>
                     <div className='synopsisMain'>
                         <div className='empty'></div>
-                        <div className='synopsis'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        In magnam suscipit, at id facere quasi ducimus porro expedita soluta quia asperiores maxime molestias maiores iste facilis enim reiciendis,
-                        culpa consequuntur!<br></br>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br></br>
-                        Excepturi animi est ipsa eos earum atque tenetur perspiciatis temporibus ad fugiat iure laborum ullam,<br></br>
-                        error quos magni sapiente maxime nobis consequuntur?
+                        <div className='synopsis'>{props.movie.overview}
                         </div>
                     </div>
                 </div>  
@@ -46,11 +45,12 @@ const Movie = (props) => {
             <div className='midRight'>
                 <div className='midRightTop'>
                     
-                    <div className='poster'></div>
+                    <div className='poster'><img className='posterImg' src={props.movie.poster_path_hd} alt={props.movie.poster_path}></img>
+                    </div>
                     <div className='movieInfo'>
-                        <div className='movieName'>Nombre:Federico y el trono del metal</div>
-                        <div className='genre'>Géneros:Suspense,Policiaca</div>
-                        <div className='director'>Director:Federico Baez Morandi</div>
+                        <div className='movieName'>Nombre: {props.movie.title}</div>
+                        <div className='genre'>Géneros: {props.movie.genres.join(', ')}</div>
+                        <div className='year'>Año: {props.movie.release_date}</div>
                     </div>
                     
                 </div>
